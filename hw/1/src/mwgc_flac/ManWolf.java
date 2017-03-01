4f3ffd873f8ac3812d4ae3fc4de6f931b2656d2b
jschmidt777 <schmidtjoseph39@gmail.com>
2017-03-01 00:06:41 -0500

    Created the two classes ManWolf and driverDFA.

---
+++ hw/1/src/mwgc_flac/ManWolf.java
@@ -0,0 +1,23 @@
+/**
+ * file: ManWolf.java
+ * author: Joseph Schmidt
+ * course: CMPT 440
+ * assignment: Homework 1
+ * due date: March 20, 2017
+ * version: 1.0
+ * 
+ * This file takes a string from the command line 
+ * and reports whether or not it represents a solution to the man-wolf-goat-cabbage problem
+ * in Chapter 2 of the textbook.
+ */
+package mwgc_flac;
+
+
+public class ManWolf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hw/1/src/mwgc_flac/driverDFA.java
@@ -0,0 +1,16 @@
+/**
+ * file: driverDFA.java
+ * author: Joseph Schmidt
+ * course: CMPT 440
+ * assignment: Homework 1
+ * due date: March 20, 2017
+ * version: 1.0
+ * 
+ * This file reads from standard input, calls the functions of
+ * the ManWolf class, and prints the result to standard output.
+ */
+package mwgc_flac;
+
+public class driverDFA {
+
+}
