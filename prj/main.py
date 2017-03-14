14bde32c81d95dfb4ee7b7dd4fbde339026c8a4e
jschmidt777 <schmidtjoseph39@gmail.com>
2017-03-14 18:24:05 -0400

    Validating inputted ACLs.

--- prj/main.py
+++ prj/main.py
@@ -1,8 +1,10 @@
 # Nullifies circular dependencies when loaded in packages.
 from node import Node
-from readacl import lines
+from readacl import lines, permits, denys
 newNode = Node()
 print(newNode)
 newNode1 = Node(acceptedState=True, nodeId=5)
 print(newNode1)
 print(lines)
+print(permits)
+print(denys)
--- prj/readacl.py
+++ prj/readacl.py
@@ -2,6 +2,20 @@
 # this will check to see if the input is valid as well
 # where next thing after a permit or deny is not x.x.x.x or any, errror
 # if no deny any or permit any, add deny any to the end of the file
+# is there a way to make this more secure?
+# Only take a file from the acl folder as an argument when executing
+import sys
+
 with open('acl/acl0.txt') as acl:
     lines = acl.readlines()
 lines = [x.strip('\n') for x in lines]
+lines = list(filter(None, lines))  # gets rid of empty strings
+
+if len(lines) == 0:
+    sys.exit("Error: The ACL provided was empty! " +
+             "Please provide a usable ACL (See rules.txt for ACL rules).")
+
+permits = [x for x in lines if 'permit' in x]
+denys = [x for x in lines if 'deny' in x]
+# depending on the first line, if it's permit,
+# check for deny at end and add if needed
