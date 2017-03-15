787f4520f09c5e3bd2a844d9f98c0336b188d1a1
jschmidt777 <schmidtjoseph39@gmail.com>
2017-03-14 23:16:30 -0400

    ACL validation and test cases.

--- prj/dfa.py
+++ prj/dfa.py
@@ -1,1 +1,2 @@
 # from node import Node
+# TODO: Verbose mode for running pcap file src ips against dfa(s)
--- prj/main.py
+++ prj/main.py
@@ -1,10 +1,10 @@
 # Nullifies circular dependencies when loaded in packages.
-from node import Node
+# from node import Node
+# newNode = Node()
+# print(newNode)
+# newNode1 = Node(acceptedState=True, nodeId=5)
+# print(newNode1)
 from readacl import lines, permits, denys
-newNode = Node()
-print(newNode)
-newNode1 = Node(acceptedState=True, nodeId=5)
-print(newNode1)
 print(lines)
 print(permits)
 print(denys)
