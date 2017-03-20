ecf7c88d044aea9d46423768ca83a63b9b623db5
jschmidt777 <schmidtjoseph39@gmail.com>
2017-03-20 11:57:59 -0400

    Fixed some of the comments and styling

--- hw/1/src/driverDFA.java
+++ hw/1/src/driverDFA.java
@@ -6,36 +6,38 @@
  * due date: March 20, 2017
  * version: 1.0
  * 
- * This file reads from standard input, calls the functions of
- * the ManWolf class, and prints the result to standard output.
+ * This file reads an argument from the user, calls the functions of
+ * the ManWolf class, and prints the result to the command line.
  */
 import java.io.IOException;
+
+
 /**
  * driverDFA
  * 
  * This class creates an instance of the class ManWolf and 
  * then reads the argument provided by the user of a possible
  * solution to the man-wolf-goat-cabbage problem. It then tells
- * the user whether or not the solution is correct or not.
+ * the user whether or not the solution is correct.
  */
 public class driverDFA {
   public static void main(String[] args) throws IOException{
-      try{ 
-        ManWolf d = new ManWolf(); // The dfa created by the ManWolf class
-        String input = args[0]; //This reads an argument from the command line
-        while (input != null) {
-            d.reset();
-            d.process(input);
-            if (d.accepted()) {
-              System.out.println("That is a solution.");
-              System.exit(0);
-            }else{
-              System.out.println("That is not a solution.");
-              System.exit(0);
-            }
+    try{ 
+      ManWolf d = new ManWolf(); // The dfa created by the ManWolf class
+      String input = args[0]; //This reads an argument from the command line
+      while (input != null) {
+        d.reset();
+        d.process(input);
+        if (d.accepted()) {
+          System.out.println("That is a solution.");
+          System.exit(0);
+        }else{
+          System.out.println("That is not a solution.");
+          System.exit(0);
         }
-      }catch (ArrayIndexOutOfBoundsException ex){
-        System.out.println("Error: Please provide an argument.");
       }
+    }catch (ArrayIndexOutOfBoundsException ex){
+      System.out.println("Error: Please provide an argument.");
     }
+  }
 }
